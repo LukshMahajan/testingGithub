c3f557639faac63d734e647f118445342fd8daa1
Durvp <113187668+Durvp@users.noreply.github.com>
2024-02-20 14:01:21 -0500

    Update Luksh_Mahajan.java

--- FirstGithubProject/src/Luksh_Mahajan.java
+++ FirstGithubProject/src/Luksh_Mahajan.java
@@ -1,5 +1,7 @@
 public class Luksh_Mahajan {
     public static void main(String args[]){
         System.out.println("HI LUKSH I LUVVV LUVV LUVVV UUUUUU");
+        System.out.println("HI Luksh ");
+        System.out.println("HI Mateo ");
     }
 }
